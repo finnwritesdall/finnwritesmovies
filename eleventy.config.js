ed71570d6a7a658e108dd1a11861b9f266b1dfb1
finnwritesdall <finnwritesdall@gmail.com>
2025-07-04 23:51:25 +1000

    changed feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -29,7 +29,7 @@ export default async function (eleventyConfig) {
 		outputPath: "/feed.xml",
 		collection: {
 			name: "all", // iterate over `collections.posts`
-			limit: 10,     // 0 means no limit
+			limit: 0,     // 0 means no limit
 		},
 		metadata: {
 			language: "en",
